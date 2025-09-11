665d8044bf6d4b9d4c390d6c64544f8d851ac6cb
firsenLargE <codeszion@gmail.com>
2025-09-11 12:53:32 +0545

    1

---
+++ numbers/Decimal.java
@@ -0,0 +1,6 @@
+class Decimal {
+    public static void main(String[] args) {
+        int number = 23234343;
+        System.out.printf("The number is : %,d " , number);  //using commas in betweenn the integer + is replaced with a comma
+    }
+}
