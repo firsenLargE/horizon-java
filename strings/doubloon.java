34f5c6d0304cb0738c21809a2522272270a2243e
firsenLargE <codeszion@gmail.com>
2025-09-27 17:35:23 +0545

    ok

--- strings/doubloon.java
+++ strings/doubloon.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 /*
  * A word is said to be a “doubloon” if every letter that appears
 in the word appears exactly twice. Here are some example doubloons found
@@ -9,8 +11,33 @@ shoer, intestines, Isis, mama, Mimi, murmur, noon, Otto, papa,
 peep, reappear, redder, sees, Shanghaiings, Toto
  */
 
-
 public class doubloon {
-    
-    
+    public static void main(String[] args) {
+        System.out.println("Enter word to check doubloon!:");
+
+        Scanner sc = new Scanner(System.in);
+        String vic = sc.nextLine();
+        System.out.println(isDboubloon(vic));
+        sc.close();
+
+    }
+
+    public static boolean isDboubloon(String a) {
+        int[] count = new int[250];
+        String b = a.toLowerCase();
+        for (int i = 0; i < b.length(); i++) {
+            char c = b.charAt(i);
+            if (Character.isLetter(c)) {
+                count[c - 'a']++;
+            }
+
+        }
+        for (int counts : count) {
+            if (counts != 0 && counts != 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
