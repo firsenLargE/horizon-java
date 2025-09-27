b93f3d0f07fd4d4add92e5e59c9603b1a3cf857f
firsenLargE <codeszion@gmail.com>
2025-09-27 20:01:47 +0545

    ok

--- strings/NumberOfLetters.java
+++ strings/NumberOfLetters.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
 
 //suppose we are given a word of length n and we have to find the number of particular words in the given word
 
-
 public class NumberOfLetters {
     public static void main(String[] args) {
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the word:");
-        String word = in.nextLine();
-        System.out.println("Enter the letter to count:");
-        String letter = in.next();
 
-        String toCheck = word.toLowerCase();
-
-        for(char c = 'a';c<='z';c++){
-            int total = 0 ;
-            for(int i = 0;i<toCheck.length();i++){
-                if(letter == c){
-                    total++;
-                }
-                System.out.println(total);
-            }
-            
-
-
-        }
-        
+        countLetter();
 
     }
+
+    public static void countLetter() {
+        System.out.println("Enter the word to compute:");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.println("Enter the letter to count:");
+        char letter = sc.next().charAt(0);
+
+        String start = word.toLowerCase();
+        char end = Character.toLowerCase(letter);
+
+        int count = 0;
+        for (int i = 0; i < start.length(); i++) {
+            if (start.charAt(i) == end) {
+                count++;
+            }
+        }
+        System.out.println("Total number" + letter +"has been repeated is :"+count);
+
+    }
+
 }
